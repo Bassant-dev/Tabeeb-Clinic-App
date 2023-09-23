61c36693bc30595454a26a84972243c2bbee487f
Salmakhaled123 <toty5alouda@gmail.com>
2023-09-24 01:41:53 +0300

    add_custom_time_item

--- lib/screens/doctor_details/views/doctor details view.dart
+++ lib/screens/doctor_details/views/doctor details view.dart
@@ -160,37 +160,3 @@ class DoctorDetailsView extends StatelessWidget {
   }
 }
 
-class TimeItem extends StatelessWidget {
-  const TimeItem({
-    Key? key,
-    required this.cubit,
-    required this.time,
-    required this.isActive,
-    required this.onTap,
-  }) : super(key: key);
-  final String time;
-  final DoctorDetailsCubit cubit;
-  final bool isActive;
-  final void Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 80.w,
-        height: 30.h,
-        decoration: BoxDecoration(
-            color: isActive
-                ? AppColors.primaryColor
-                : const Color(0xff030e191a).withOpacity(0.1),
-            borderRadius: BorderRadius.circular(24)),
-        child: Center(
-            child: Text(
-          time,
-          style: const TextStyle(color: Colors.white),
-        )),
-      ),
-    );
-  }
-}
